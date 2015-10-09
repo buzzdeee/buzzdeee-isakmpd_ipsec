fc68fd94cbee65c0052676d41c7e615bd1c44e87
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-10-09 16:13:05 +0200

    Set sysctl values for ah, esp, ipcomp, based on service_enable in service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,20 @@ class isakmpd_ipsec::service (
   $isakmpd_ensure,
   $isakmpd_flags,
 ) {
+
+  if $isakmpd_ensure == 'running' {
+    $value = 1
+  } else {
+    $value = 0
+  }
+
+  Sysctl::Value { value => $value }
+
+  sysctl::value { 'net.inet.ah.enable': }
+  sysctl::value { 'net.inet.esp.enable': }
+  sysctl::value { 'net.inet.ipcomp.enable': }
+
+
   service { 'isakmpd':
     ensure => $isakmpd_ensure,
     flags  => $isakmpd_flags,
